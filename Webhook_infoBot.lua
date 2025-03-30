d4b93c1c0f8a595519b456e187c8c95b71a66f2f
Evan0A <wlsjordan01@gmail.com>
2025-03-30 13:41:08 +0700

    Update Webhook_infoBot.lua

--- Webhook_infoBot.lua
+++ Webhook_infoBot.lua
@@ -1,13 +1,16 @@
 function botInfo(info)
     colour = 0 
+    bstatus = ""
     stats = ""
     fossill = fossil[getBot():getWorld().name] or 0
-    if getBot().status == "online" then
+    if getBot().status == 1 then
         colour = 65280
         stats = "<a:Online1:1355754776955977788>"
+        bstatus = "Online"
     else
         colour = 16711680
         stats = "<a:offline:1355765716267171850>"
+        bstatus = "Offline"
     end
     webhook = Webhook.new(webhookLink)
     
@@ -15,7 +18,7 @@ function botInfo(info)
     webhook.embed1.color = colour
     webhook.embed1.title = "<:pickaxe:1011931845065183313> ROTATION BOT STATUS: "..info
     webhook.embed1:addField(":robot: ] Bot's name: ", "||"..getBot().name.."||")
-    webhook.embed1:addField(stats.." ] Bot's status: ", getBot().status, true)
+    webhook.embed1:addField(stats.." ] Bot's status: ", bstatus, true)
     webhook.embed1:addField("<:ruby:1355776434219651243> ] Bot's gems:", getBot().gem_count, true)
     webhook.embed1:addField("<:Earth_Minecraft:1355777709443121212> ] Bot's world: ", "||"..getBot():getWorld().name.."||", true)
     webhook.embed1:addField("<:fossil_rock:1011972962573881464> ] Fossil: ")
