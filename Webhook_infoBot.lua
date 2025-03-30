a959bdc086e99ce7ce140c75fca375b7ebecd53c
Evan0A <wlsjordan01@gmail.com>
2025-03-30 13:16:13 +0700

    Create Webhook_infoBot.lua

---
+++ Webhook_infoBot.lua
@@ -0,0 +1,28 @@
+function botInfo(info)
+    colour = 0 
+    stats = ""
+    fossill = fossil[getBot():getWorld().name] or 0
+    if getBot().status == "online" then
+        colour = 65280
+        stats = "<a:Online1:1355754776955977788>""
+    else
+        colour = 16711680
+        stats = "<a:offline:1355765716267171850>"
+    end
+    webhook = Webhook.new(webhookLink)
+    
+    webhook.embed1.use = true
+    webhook.embed1.color = colour
+    webhook.embed1.title = "<:pickaxe:1011931845065183313> ROTATION BOT STATUS: "..info
+    webhook.embed1:addField(":robot: ] Bot's name: ", "||"..getBot().name.."||")
+    webhook.embed1:addField(stats.." ] Bot's status: ", getBot().status, true)
+    webhook.embed1:addField("<:ruby:1355776434219651243> ] Bot's gems:", getBot().gem_count, true)
+    webhook.embed1:addField("<:Earth_Minecraft:1355777709443121212> ] Bot's world: ", "||"..getBot().getWorld().name.."||", true)
+    webhook.embed1:addField("<:fossil_rock:1011972962573881464> ] Fossil: ")
+    webhook.embed1.footer.text = "]]"..os.date("!%a %b %d, %Y at %I:%M %p", os.time() + 7 * 60 * 60).."[["
+    if messageId ~= "" then
+        webhook:edit(messageId)
+    else
+        webhook:send()
+    end
+end
