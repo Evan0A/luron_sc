aae769202b39b060c6884d812ff34eda2c31e8e1
Evan0A <wlsjordan01@gmail.com>
2025-04-03 11:50:07 +0700

    Update kaca_jendela.lua

--- kaca_jendela.lua
+++ kaca_jendela.lua
@@ -1031,7 +1031,7 @@ function plant(world)
         end
 
         if not dontPlant and (getBot():getWorld():hasAccess(tile.x, tile.y) and tile.fg == 0 and getBot():getWorld():getTile(tile.x, tile.y + 1).fg ~= 0 and getBot():getWorld():getTile(tile.x, tile.y + 1).fg ~= itmId and getBot():getWorld():getTile(tile.x, tile.y + 1):hasFlag(0) and getBot():getWorld():getTile(tile.x, tile.y + 1).fg ~= 12 and tile.fg ~= itmSeed) then
-            if not blacklistTile or check(tile.x,tile.y)) then
+            if not blacklistTile or check(tile.x,tile.y) then
 
                 getBot():findPath(tile.x,tile.y)
 
