1ee5e0e4dfea277a23a5327385f4cbeb8dad092d
Evan0A <wlsjordan01@gmail.com>
2025-07-17 13:47:40 +0700

    Update rest_malady_source.lua

--- rest_malady_source.lua
+++ rest_malady_source.lua
@@ -1,12 +1,12 @@
 print("VERSION: 15")
 ---[=== CONFIG ===]---
-auto_rest_many_mods = true
+auto_rest_many_mods = false
 minimum_many_mods = 5
 
-auto_rest_specific_mod = true
+auto_rest_specific_mod = false
 specific_mod_list = {"kailyx", "misthios", "windyplay"} -- uppercase is not nessesary
 
-auto_rest_schedule = true
+auto_rest_schedule = false
 schedule_zone = "UTC+7"
 schedule_list = {
     "14:00 - 15:00",
