d9b1e7739f8f1ea4dfc08c003f8d0a2f230e1d42
Evan0A <wlsjordan01@gmail.com>
2025-08-30 19:39:20 +0700

    Update rest2_full.lua

--- rest2_full.lua
+++ rest2_full.lua
@@ -1,4 +1,4 @@
-print("VERSION 2")
+print("VERSION 1")
 ---[=== CONFIG ===]---
 auto_rest_many_mods = true
 minimum_many_mods = 5
@@ -208,8 +208,7 @@ function webhookRest(nameBot, from)
             nameBot = nameBot.."("..tostring(getBot(nameBot).index)..")"
         end
         local emoji = reason_emoji[tostring(from)] or "❓"
-        local utc_time = os.time(os.date("!*t"))
-        utc_time = utc_time + (tonumber(schedule_zone) * 3600)
+        local utc_time = (os.time(os.date("!*t"))) + (tonumber(schedule_zone) * 3600)
         -- Info tambahan
         local extra_info = ""
         if from == 1 then
